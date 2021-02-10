c02f408c58b9c1929fa3bbd9ef51cba7e9e55424
shouvikbasu <babanomania@gmail.com>
2021-02-10 13:01:46 +0530

    removed icons enabled shortcuts

--- pages/apps.js
+++ pages/apps.js
@@ -3,12 +3,18 @@ import shortcuts from "../shortcut.config";
 
 import Card from "../components/card";
 
+function link(sLink) {
+  return sLink.startsWith(":")
+    ? `${window.location.href.split(":")[1]}:${sLink.split(":")[1]}`
+    : sLink;
+}
+
 export default function Home() {
   return (
     <Layout active="Applications">
       <div className="grid gap-4 grid-cols-2 md:grid-cols-4 mt-5 mx-2">
         {shortcuts.map((skt) => (
-          <a href={skt.link}>
+          <a href={link(skt.link)}>
             <Card title={skt.title}>
               <span className="flex justify-center">
                 <img src={skt.logo} className="w-16 h-16"></img>
