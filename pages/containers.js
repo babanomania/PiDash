9ad0595da5e1de78ab77e3bd6546ba969fb56946
shouvikbasu <babanomania@gmail.com>
2021-02-10 13:46:09 +0530

    improved status

--- pages/containers.js
+++ pages/containers.js
@@ -9,9 +9,13 @@ export default function Conatiner({ docker }) {
       <div className="grid gap-4 grid-cols-2 md:grid-cols-4 mt-5 mx-2">
         {docker.map((dkr) => (
           <Card title={dkr.name}>
-            <span className="flex justify-center">
-              <div>{dkr.image}</div>
-              <div>{dkr.state}</div>
+            <span className="flex flex-coljustify-center">
+              <div className="text-base font-bold">
+                <b>Name</b> {dkr.image}
+              </div>
+              <div className="text-base">
+                <b>Status</b> {dkr.state}
+              </div>
             </span>
           </Card>
         ))}
